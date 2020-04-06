73258c150baad3a7c0411cc7c3d84ddc155353a8
MasterRoman <53902992+MasterRoman@users.noreply.github.com>
2020-04-06 18:35:04 +0300

    feat:solve problem

--- RSSchool_T3/RSSchool_T3/2/Combinator.m
+++ RSSchool_T3/RSSchool_T3/2/Combinator.m
@@ -2,6 +2,28 @@
 
 @implementation Combinator
 - (NSNumber*)chechChooseFromArray:(NSArray <NSNumber*>*)array {
-    return @0;
+    NSUInteger m = [[array firstObject] intValue];
+    NSUInteger n = [[array lastObject] intValue];
+    NSUInteger res = 1;
+    if (m == n) {
+        return @1;
+    }
+    NSUInteger factN = [Combinator fact:n];
+    while (res <= n) {
+        NSUInteger cur = factN / ( [Combinator fact:(n - res)] * [Combinator fact:res]);
+        // Combinations
+        if (cur == m) {
+            return [[NSNumber alloc] initWithInt: (int)res];
+        }
+        res += 1;
+    }
+    return nil;
+}
++ (long)fact:(NSUInteger) n {
+    long f = 1;
+    for (int i=1; i<n+1; i++) {
+        f = f * i;
+    }
+    return f;
 }
 @end
